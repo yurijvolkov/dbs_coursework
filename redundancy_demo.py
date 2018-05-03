413f853c9b07e2d114dda8e8a5e117c52c30c5e6
Yurij Volkov <yurijvolkov1@yandex.ru>
2018-05-04 00:13:01 +0300

    Demo added.

--- redundancy_demo.py
+++ redundancy_demo.py
@@ -2,15 +2,15 @@ import sys
 
 from requests import get, post
 
-def create_record_demo():
+def create_record_demo(s):
     return post('http://localhost:8045/paths',
-                json='{"name": "ToUniversity", "nodes": [1,2,3]}')
+                json='{"name": "%s", "nodes": [1,2,3]}' % s)
 
-def get_record_demo():
-    return get('http://localhost:8045/paths?path_name=ToUniversity').text
+def get_record_demo(s):
+    return get(f'http://localhost:8045/paths?path_name=%s' % s).text
 
-def delete_record_demo():
-    return delete('http://localhost:8045/paths?path_name=ToUniversity') 
+def delete_record_demo(s):
+    return delete('http://localhost:8045/paths?path_name=%s' % s) 
 
 
 
@@ -19,10 +19,10 @@ if __name__ == '__main__':
     n = int(sys.argv[1])
 
     if n == 0:
-        print(create_record_demo())
+        print(create_record_demo(sys.argv[2]))
     elif n == 1:
-        print(get_record_demo())
+        print(get_record_demo(sys.argv[2]))
     elif n == 2:
-        print(delete_record_demo())
+        print(delete_record_demo(sys.argv[2]))
 
 
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-python redundancy_demo.py 0
+python redundancy_demo.py 0 TestName
 docker kill docker_cassandra1_1
 docker kill docker_cassandra2_1
 
-python redundancy_demo.py 1
+python redundancy_demo.py 1 TestName
